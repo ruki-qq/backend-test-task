2253eaed42fb16403a9e52b08d703eb95cab34de
ruki-qq <ruki@duck.com>
2025-08-24 19:37:06 +0300

    add docstrings to channels routes

--- src/app/routers/api/channels.py
+++ src/app/routers/api/channels.py
@@ -13,6 +13,8 @@ router = APIRouter(prefix="/channels", tags=["channels"])
 
 @router.post("/", response_model=ChannelResponse)
 async def create_channel(channel_data: ChannelCreate) -> ChannelResponse:
+    """Создать канал"""
+
     try:
         return await ChannelService.create_channel(channel_data)
     except (InvalidId, TypeError) as e:
@@ -27,6 +29,7 @@ async def create_channel(channel_data: ChannelCreate) -> ChannelResponse:
 async def get_channels(
     chat_bot_id: str | None = None, active: bool | None = None
 ) -> List[ChannelResponse]:
+    """Получить список каналов по id чат бота и/или флагу активности"""
     try:
         channels = await ChannelService.get_channels_by_chatbot(chat_bot_id, active)
     except (InvalidId, TypeError) as e:
@@ -41,6 +44,8 @@ async def get_channels(
 
 @router.get("/{channel_id}", response_model=ChannelResponse)
 async def get_channel(channel_id: str) -> ChannelResponse:
+    """Получить канал по его id"""
+
     try:
         channel = await ChannelService.get_channel(channel_id)
     except (InvalidId, TypeError) as e:
@@ -57,6 +62,8 @@ async def get_channel(channel_id: str) -> ChannelResponse:
 async def update_channel(
     channel_id: str, update_data: ChannelUpdate
 ) -> ChannelResponse:
+    """Обновить информацию о канале"""
+
     try:
         channel = await ChannelService.update_channel(channel_id, update_data)
     except (InvalidId, TypeError) as e:
@@ -71,6 +78,8 @@ async def update_channel(
 
 @router.delete("/{channel_id}")
 async def delete_channel(channel_id: str) -> Response:
+    """Удалить канал по id"""
+
     try:
         success = await ChannelService.delete_channel(channel_id)
     except (InvalidId, TypeError) as e:
@@ -88,6 +97,8 @@ async def delete_channel(channel_id: str) -> Response:
 
 @router.get("/{channel_id}/dialogue", response_model=List[dict])
 async def get_channel_dialogue(channel_id: str) -> List[dict]:
+    """Получить диалог из канала"""
+
     try:
         channel = await ChannelService.get_channel(channel_id)
     except (InvalidId, TypeError) as e:
